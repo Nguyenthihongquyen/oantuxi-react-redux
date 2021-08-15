eb4426981888e1f76865ce943a8a2010299eea35
duongy96@gmail.com <duongy96@gmail.com>
2021-08-16 02:52:34 +0700

    mentor review

--- src/store/reducers/oanTuXiReducer.jsx
+++ src/store/reducers/oanTuXiReducer.jsx
@@ -35,12 +35,26 @@ const oanTuXiReducer = (state = initialState, action) => {
             state.soBanChoi += 1;
             let selectedOanTuXi = state.mangDatCuoc.find(item => item.datCuoc === true);
             let thanos = state.thanos;
-            switch (selectedOanTuXi.ma) {
+            // thuật toán để check thắng hòa thua như sau:
+            // tạo ra 1 list [keo,bua,bao]; mỗi lần người dùng chọn hoặc máy chọn thì dùng hàm findIndex để lấy index ra
+            // để ý thấy cái mảng này, thì index của nó có quy luật thắng thua như sau
+            // mình chọn cái gì bạn chọn đúng cái đó thì hòa, cái này dễ
+            // mình chọn bất cứ cái gì, nếu bạn chọn sau mình 1 index, mình thua ("bua" thắng "keo", "bao" thang "bua"),
+            // hoặc bạn chọn trước mình 2 đơn vị mình cũng thua(mình chọn "bao" bạn chọn "keo")
+            // if(indexYou===indexMe){
+            //     result ="DRAW";
+            // }else if(indexMe===indexYou-1|| indexMe === indexYou+2){
+            //     result ="LOSE";
+            // }else{
+            //     result ="WIN";
+            // }
+
+            switch (selectedOanTuXi.ma) {//dùng thuật toán như bên trên để đỡ switch case mệt mỏi nha e, còn làm biếng đọc thì thôi a xin lỗi phiền e rồi :))
                 case 'keo':
                     if (thanos.ma === 'keo') {
                         state.ketQua = 'Hòa >.<';
                     } else if (thanos.ma === 'bua') {
-                        state.ketQua = 'Thua sắp mặt luôn =((((';
+                        state.ketQua = 'Thua sắp mặt luôn =(((('; //xàm
                     } else {
                         state.soBanThang += 1;
                         state.ketQua = "I'm iron man, i love you 3000!!";
